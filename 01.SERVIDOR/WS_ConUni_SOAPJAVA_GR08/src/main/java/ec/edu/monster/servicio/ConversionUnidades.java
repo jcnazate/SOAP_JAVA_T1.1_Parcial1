d28bb571fdc8c5b317eedbb5ef4bbe17aa686037
Jcnazate11 <jcnazate@espe.edu.ec>
2025-10-15 12:51:37 -0500

    update conversiones

--- 01.SERVIDOR/WS_ConUni_SOAPJAVA_GR08/src/main/java/ec/edu/monster/servicio/ConversionUnidades.java
+++ 01.SERVIDOR/WS_ConUni_SOAPJAVA_GR08/src/main/java/ec/edu/monster/servicio/ConversionUnidades.java
@@ -28,4 +28,39 @@ public class ConversionUnidades {
     public double centimetrosAPulgadas(@WebParam(name = "centimeros") double centimeros) {
         return centimeros / 2.54;
     }
+     /**
+     * WebServices Operations 1
+     */
+    @WebMethod(operationName = "kilogramosAGramos")
+    public double kilogramosAGramos(@WebParam(name = "kilogramos") double kilogramos) {
+        return kilogramos * 1000.0;
+    }
+    
+      /**
+     *WebServices Operations 2
+     * Masa: g -> kg
+     */
+    @WebMethod(operationName = "gramosAkilogramos")
+    public double gramosAkilogramos(@WebParam(name = "gramos") double gramos) {
+        return gramos / 1000.0;
+    }
+
+    /**
+     * WebServices Operations 3
+     * Temperatura: °C -> K
+     * Fórmula: K = °C + 273.15
+     */
+    @WebMethod(operationName = "celsiusAKelvin")
+    public double celsiusAKelvin(@WebParam(name = "celsius") double celsius) {
+        return celsius + 273.15;
+    }
+     /**
+     * WebServices Operations 3
+     * Temperatura: K -> °C
+     * Fórmula: K = °C + 273.15
+     */
+    @WebMethod(operationName = "kelvinAcelcius")
+    public double kelvinAcelcius(@WebParam(name = "kelvin") double kelvin) {
+        return kelvin - 273.15;
+    }
 }
